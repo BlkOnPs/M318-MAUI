f07ac33aa1f29dc3a8d14d128cbb72cc0e29d9a8
Amel Cehic <amel.cehic@samtrevano.ch>
2025-01-28 15:58:35 +0100

    Upload esempio introduzione

---
+++ Introduzione/00Introduzione/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace _00Introduzione
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            //Evento che permette di stampare nel label il contenuto del 
+            InitializeComponent();
+        }
+
+        private void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(entName.Text)) {
+                lbName.Text = "Ciao a tutti";
+            }
+            else
+            {
+                lbName.Text = entName.Text;
+            }
+        }
+    }
+
+}

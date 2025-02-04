3cf7a7a1e21abb586b822927bf5bcb7de8941ac4
Amel Cehic <amel.cehic@samtrevano.ch>
2025-02-04 14:38:12 +0100

    Upload esercizio 04LayoutProva

---
+++ Introduzione/04Layout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _04Layout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Introduzione/04Layout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _04Layout
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

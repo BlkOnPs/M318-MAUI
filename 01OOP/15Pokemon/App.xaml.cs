8e59cd496dd6e686000b44d75d56ee3ba7024973
Amel Cehic <amel.cehic@samtrevano.ch>
2025-04-18 17:05:27 +0200

    Upload esercizio 15Pokemon

---
+++ 01OOP/15Pokemon/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _15Pokemon
+{
+    public partial class App : Application
+    {
+        public App ( )
+        {
+            InitializeComponent ();
+
+            MainPage = new AppShell ();
+        }
+    }
+}
---
+++ 01OOP/15Pokemon/Models/Ability.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15Pokemon.Models
+{
+    internal class Ability
+    {
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+
+		private string _type;
+
+		public string Type
+		{
+			get { return _type; }
+			set { _type = value; }
+		}
+
+        public Ability ( string description, string type )
+        {
+            Description = description;
+            Type = type;
+        }
+
+        public override string? ToString ( )
+        {
+            return Type + "\n" + Description;
+        }
+
+    }
+}
